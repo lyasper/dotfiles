ccd72f63b71d90c3f79b80898459a0eb1d29325a
li yang <liyang@localhost.(none)>
2012-07-09 18:14:52 +0800

    add emacs config

---
+++ emacs.d/func.el
@@ -0,0 +1,37 @@
+
+;; ===== Function to delete a line =====
+
+;; First define a variable which will store the previous column position
+(defvar previous-column nil "Save the column position")
+
+;; Define the nuke-line function. The line is killed, then the newline
+;; character is deleted. The column which the cursor was positioned at is then
+;; restored. Because the kill-line function is used, the contents deleted can
+;; be later restored by usibackward-delete-char-untabifyng the yank commands.
+(defun nuke-line()
+  "Kill an entire line, including the trailing newline character"
+  (interactive)
+
+  ;; Store the current column position, so it can later be restored for a more
+  ;; natural feel to the deletion
+  (setq previous-column (current-column))
+
+  ;; Now move to the end of the current line
+  (end-of-line)
+
+  ;; Test the length of the line. If it is 0, there is no need for a
+  ;; kill-line. All that happens in this case is that the new-line character
+  ;; is deleted.
+  (if (= (current-column) 0)
+    (delete-char 1)
+
+    ;; This is the 'else' clause. The current line being deleted is not zero
+    ;; in length. First remove the line by moving to its start and then
+    ;; killing, followed by deletion of the newline character, and then
+    ;; finally restoration of the column position.
+    (progn
+      (beginning-of-line)
+      (kill-line)
+      (delete-char 1)
+      (move-to-column previous-column))))
+
