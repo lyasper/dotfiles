52e8d069990159ddac28040bf7e5cb8f5ee4f2c2
LiYang <liyang@and-dev02.(none)>
2012-08-03 23:37:59 +0800

    add createlink and some alias

---
+++ createlink.sh
@@ -0,0 +1,25 @@
+#!/bin/bash -x
+
+FROM=$PWD
+TO=$HOME
+
+
+if [ -f $TO/bashrc.d ]; then
+    ln -s $FROM/bashrc.d $TO/bashrc.d
+fi
+ln -s $FROM/dot_emacs  $TO/.emacs
+ln -s $FROM/gitconfig  $TO/gitconfig
+if [ -f $TO/emacs.d ]; then
+    ln -s $FROM/emacs.d   $TO/emacs.d 
+fi
+ln -s $FROM/bashrc_local  $TO/.bashrc_local
+
+
+#if [ -f ~/.bashrc ]; then
+#cat <<EOF >> $TO/.bashrc  
+#if [ -f ~/.bashrc_local ]; then
+#    . ~/.bashrc_local
+#fi
+#
+#EOF
+#fi
