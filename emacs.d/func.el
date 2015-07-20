8ce7969cea4dba7a566a840d85ab08ce69104e85
who <who@example.com>
2015-07-20 22:30:57 +0800

    Add date function

--- emacs.d/func.el
+++ emacs.d/func.el
@@ -21,3 +21,19 @@ With a prefix argument, insert a newline above the current line."
   (if abovep
       (vi-open-line-above)
     (vi-open-line-below)))
+
+(defun insert-date ()
+  "Insert current date yyyy-mm-dd."
+  (interactive)
+  (insert (format-time-string "%Y-%m-%d")))
+
+(defun insert-date-time ()
+  "Insert current date-time string in full ISO 8601 format.
+Example: 2010-11-29T23:23:35-08:00"
+  (interactive)
+  (insert
+   (concat
+    (format-time-string "%Y-%m-%dT%T")
+    ((lambda (x) (concat (substring x 0 3) ":" (substring x 3 5)))
+     (format-time-string "%z")))))
+
